b39bdf536887cf62bfcadb6c7e29f49b27d6a5c5
Steve <mcgongalstephen@gmail.com>
2017-05-18 19:14:37 +0100

    search

--- client/filters.py
+++ client/filters.py
@@ -3,6 +3,10 @@ import django_filters
 from .models import Client
 
 class ClientFilter(django_filters.FilterSet):
+    forename = django_filters.CharFilter(lookup_expr='icontains')
+    middle_name = django_filters.CharFilter(lookup_expr='icontains')
+    surname = django_filters.CharFilter(lookup_expr='icontains')
+    year_of_birth = django_filters.NumberFilter(name='dob', lookup_expr='year')
     class Meta:
         model = Client
-        fields = ['forename', 'surname', ]
+        fields = ['forename', 'surname']
