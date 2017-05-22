18647de5f4ce3dfa99aa2e2bd103916eb41fb036
Deploy script <andrew.wiseman@cottages-and-castles.co.uk>
2017-05-22 17:17:08 +0100

    contract

--- client/views/__init__.py
+++ client/views/__init__.py
@@ -1,3 +1,4 @@
 # https://stackoverflow.com/questions/1921771/django-split-views-py-in-several-files
+# https://stackoverflow.com/questions/20010991/split-views-py-into-multiple-files
 from views_client_details import home_page, quick_client_search, client_search, client_detail, client_new, client_edit
 from views_migration import load_clients
