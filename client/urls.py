dad67757f90b7a4c2431a851bb87ce1a6d242a21
Steve <mcgongalstephen@gmail.com>
2017-05-23 15:00:37 +0100

    setup

--- client/urls.py
+++ client/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # client crud
     url(r'^client/new/$', views.client_new, name='client_new'),
     url(r'^client/(?P<pk>\d+)/edit/$', views.client_edit, name='client_edit'),
     url(r'^client/(?P<pk>\d+)/$', views.client_detail, name='client_detail'),
     url(r'^client_search/$', views.client_search, name='client_search'),
+    # contract crud
+    # url(r'^contract/new/$', views.client_new, name='contract_new'),
+    # searching
     url(r'^auto/quick_client_search/$', views.quick_client_search, name='quick_client_search'),
+    # migration
     url(r'^load_clients/$', views.load_clients, name='load_clients'),
 ]
--- client/views/__init__.py
+++ client/views/__init__.py
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/questions/1921771/django-split-views-py-in-several-files
 # https://stackoverflow.com/questions/20010991/split-views-py-into-multiple-files
-from .views_client_details import *
+from .client_crud import *
 from .migration import *
 from .client_searching import *
